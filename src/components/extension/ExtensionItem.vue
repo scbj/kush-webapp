1cb3281500879657d33239fd2856143fc36c719f
Sacha Bejaud <sacha.bejaud@outlook.com>
2020-02-21 22:57:06 +0100

    Create ExtensionItem component

---
+++ src/components/extension/ExtensionItem.vue
@@ -0,0 +1,64 @@
+<template>
+  <li class="extension-item">
+    <div class="content">
+      <div class="icon"></div>
+      <span class="name">Home</span>
+      <div class="peoples"></div>
+      <button class="button-options"></button>
+    </div>
+  </li>
+</template>
+
+<style lang="scss" scoped>
+.extension-item {
+  width: 163px;
+  height: 94px;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  box-shadow: 0px 2px 15px #03030D14;
+  border-radius: 8px;
+  opacity: 1;
+  padding: 14px;
+}
+
+.content {
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  grid-template-rows: auto 1fr auto;
+  grid-gap: 12px;
+}
+
+.content .icon {
+  width: 20px;
+  height: 20px;
+  background: #eee;
+  border-radius: 3px;
+}
+
+.content .name {
+  text-align: left;
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: 0;
+  color: #2e2e2e;
+  opacity: 1;
+  align-self: center;
+  grid-column: 2 / 4;
+}
+
+.content .peoples {
+  width: 88px;
+  height: 16px;
+  background: #eee;
+  grid-row: 3 / 4;
+  grid-column: 1 / 3;
+}
+
+.content .button-options {
+  width: 3px;
+  height: 14px;
+  border: none;
+  grid-row: 3 / 4;
+  grid-column: 3 / 4;
+}
+
+</style>
--- src/views/Extensions.vue
+++ src/views/Extensions.vue
@@ -3,14 +3,7 @@
     <section class="my-extensions">
       <h2>My extensions</h2>
       <ul class="extension-list">
-        <li class="extension-item">
-          <div class="item-content">
-            <div class="icon"></div>
-            <span class="name">Home</span>
-            <div class="peoples"></div>
-            <button class="button-options"></button>
-          </div>
-        </li>
+        <ExtensionItem />
       </ul>
     </section>
     <section class="others-extensions">
@@ -18,6 +11,16 @@
     </section>
   </div>
 </template>
+
+<script>
+import ExtensionItem from '@/components/extension/ExtensionItem.vue'
+
+export default {
+  components: {
+    ExtensionItem
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .extensions-view {
@@ -37,56 +40,5 @@ h2 {
 
 .extension-list {
   display: flex;
-}
-
-.extension-item {
-  width: 163px;
-  height: 94px;
-  background: #ffffff 0% 0% no-repeat padding-box;
-  box-shadow: 0px 2px 15px #03030D14;
-  border-radius: 8px;
-  opacity: 1;
-  padding: 14px;
-}
-
-.item-content {
-  display: grid;
-  grid-template-columns: auto 1fr auto;
-  grid-template-rows: auto 1fr auto;
-  grid-gap: 12px;
-}
-
-.item-content .icon {
-  width: 20px;
-  height: 20px;
-  background: #eee;
-  border-radius: 3px;
-}
-
-.item-content .name {
-  text-align: left;
-  font-size: 16px;
-  font-weight: 600;
-  letter-spacing: 0;
-  color: #2e2e2e;
-  opacity: 1;
-  align-self: center;
-  grid-column: 2 / 4;
-}
-
-.item-content .peoples {
-  width: 88px;
-  height: 16px;
-  background: #eee;
-  grid-row: 3 / 4;
-  grid-column: 1 / 3;
-}
-
-.item-content .button-options {
-  width: 3px;
-  height: 14px;
-  border: none;
-  grid-row: 3 / 4;
-  grid-column: 3 / 4;
 }
 </style>
