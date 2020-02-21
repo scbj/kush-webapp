2ab0ad05d45fc06eb60b814fb304b76eba547a82
Sacha Bejaud <sacha.bejaud@outlook.com>
2020-02-21 22:44:55 +0100

    Create TrackItem component for the ListView slot

---
+++ src/components/tracks/TrackItem.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="track-item">
+    <div class="thumbnail"></div>
+    <span class="title">{{ data.title }}</span>
+    <span class="artist">{{ data.artist }}</span>
+    <button class="button-options"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.track-item {
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  grid-template-rows: repeat(2, 1fr);
+  height: 62px;
+}
+
+.thumbnail {
+  grid-row: 1 / 3;
+  width: 42px;
+  height: 42px;
+  background: #EEEEEE 0% 0% no-repeat padding-box;
+  opacity: 1;
+  align-self: center;
+  margin-right: 16px;
+  border-radius: 6px;
+}
+
+.title {
+  grid-row: 1 / 2;
+  grid-column: 2 / 3;
+  align-self: flex-end;
+
+  font-family: 'TT Commons';
+  font-size: 18px;
+  font-weight: 600;
+  letter-spacing: 0;
+  color: #2E2E2E;
+}
+
+.artist {
+  grid-row: 2 / 3;
+  grid-column: 2 / 3;
+  align-self: flex-start;
+
+  font-family: 'TT Commons';
+  font-size: 16px;
+  font-weight: 400;
+  letter-spacing: 0;
+  color: #6C6C6C;
+  opacity: 1;
+}
+
+.title,
+.artist {
+  justify-self: flex-start;
+}
+
+.button-options {
+  grid-row: 1 / 3;
+  grid-column: 3 / 4;
+  align-self: center;
+  width: 3px;
+  height: 14px;
+  border: none;
+}
+</style>
--- src/views/Tracks.vue
+++ src/views/Tracks.vue
@@ -6,12 +6,7 @@
     </div>
     <ListView class="tracks" :items="tracks">
       <template v-slot:item-template="{ item }">
-        <div class="track">
-          <div class="thumbnail"></div>
-          <span class="title">{{ item.title }}</span>
-          <span class="artist">{{ item.artist }}</span>
-          <button class="button-options"></button>
-        </div>
+        <TrackItem :data="item" />
       </template>
     </ListView>
   </div>
@@ -19,10 +14,12 @@
 
 <script>
 import ListView from '@/components/ListView.vue'
+import TrackItem from '@/components/tracks/TrackItem.vue'
 
 export default {
   components: {
-    ListView
+    ListView,
+    TrackItem
   },
 
   computed: {
@@ -97,62 +94,4 @@ export default {
 .tracks {
   margin: 16px;
 }
-
-.track {
-  display: grid;
-  grid-template-columns: auto 1fr auto;
-  grid-template-rows: repeat(2, 1fr);
-  height: 62px;
-
-  .thumbnail {
-    grid-row: 1 / 3;
-    width: 42px;
-    height: 42px;
-    background: #EEEEEE 0% 0% no-repeat padding-box;
-    opacity: 1;
-    align-self: center;
-    margin-right: 16px;
-    border-radius: 6px;
-  }
-
-  .title {
-    grid-row: 1 / 2;
-    grid-column: 2 / 3;
-    align-self: flex-end;
-
-    font-family: 'TT Commons';
-    font-size: 18px;
-    font-weight: 600;
-    letter-spacing: 0;
-    color: #2E2E2E;
-  }
-
-  .artist {
-    grid-row: 2 / 3;
-    grid-column: 2 / 3;
-    align-self: flex-start;
-
-    font-family: 'TT Commons';
-    font-size: 16px;
-    font-weight: 400;
-    letter-spacing: 0;
-    color: #6C6C6C;
-    opacity: 1;
-  }
-
-  .title,
-  .artist {
-    justify-self: flex-start;
-  }
-
-  .button-options {
-    grid-row: 1 / 3;
-    grid-column: 3 / 4;
-    align-self: center;
-    width: 3px;
-    height: 14px;
-    border: none;
-  }
-}
-
 </style>
